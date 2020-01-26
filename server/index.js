4ad73bbd5ddfca0a1d065496563609b50789a204
valdezDev <pvaldezdev@gmail.com>
2020-01-26 10:18:28 -0500

    implemented socket

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has launched on port ${PORT}😎😎😎`));
