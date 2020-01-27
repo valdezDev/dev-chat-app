c2c9ff750fbb341e6845d4a1145fd077ea3a49aa
valdezDev <pvaldezdev@gmail.com>
2020-01-27 14:30:03 -0500

    emitted sendMessage events in the frontend Chat.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,10 +21,10 @@ io.on('connection', (socket) => {
 
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room, ${user.room}!` });
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
-
-    socket.join(user.room);
 
     callback();
   });
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
     io.to(user.room).emit('message', { user: user.name, text: message });
 
     callback();
-  })
+  });
 
   socket.on('disconnect', () => {
     console.log('user disconnected. . . ');
