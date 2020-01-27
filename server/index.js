cf88ccd803af9b542c494176769f02c996c3c7a0
valdezDev <pvaldezdev@gmail.com>
2020-01-27 08:07:42 -0500

    added socket emits, and broadcast messaging for when a user joins a room.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -15,6 +17,16 @@ io.on('connection', (socket) => {
 
   socket.on('join', ({ name, room }, callback) => {
     console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room, ${user.room}!` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
+
+    socket.join(user.room);
+
+    callback();
   });
 
   socket.on('disconnect', () => {
