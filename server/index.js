d0246374b0d518e3b15c4ca3dd3efced7b5d96dc
valdezDev <pvaldezdev@gmail.com>
2020-01-28 12:27:47 -0500

    readying for heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
@@ -11,6 +12,9 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
+app.use(cors());
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -49,7 +53,5 @@ io.on('connection', (socket) => {
     }
   });
 });
-
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has launched on port ${PORT}😎😎😎`));
